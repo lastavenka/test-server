fb53c4884296ce0744300abebb43d5f5754b9412
Natallia Lastavenka <n.lastavenka@godeltech.com>
2019-01-22 14:38:33 +0300

    status added

--- src/routes/song.js
+++ src/routes/song.js
@@ -3,7 +3,7 @@ const generateData = require("../data");
 
 const index = function (req, res) {
   const data = generateData();
-  res.json(data);
+  return res.status(200).json(data);
 };
 
 module.exports = Router()
